fe069156fd81356ffe466a6eae16634e50a9acfa
Cameron Lund <cameronlund4@gmail.com>
2017-09-15 16:50:32 -0400

    Delete compiled lambdas

--- dynamiccompile/DynamicCompile1.java
+++
@@ -1,4 +0,0 @@
-package dynamiccompile;import club.checs.csbot.commands.CommandCall;
-import club.checs.csbot.commands.SmartCommand;public class DynamicCompile1 implements SmartCommand.OnCommand {@Override
-    public void onCommand(CommandCall call) {System.out.println("hello world!"); }
-}
--- dynamiccompile/DynamicCompile4.java
+++
@@ -1,4 +0,0 @@
-package dynamiccompile;import club.checs.csbot.commands.CommandCall;
-import club.checs.csbot.commands.SmartCommand;public class DynamicCompile4 implements SmartCommand.OnCommand {@Override
-    public void onCommand(CommandCall call) { call.sendMessage("Your words have no meaning"); try {     Thread.sleep(1000); } catch (Exception e){} call.sendMessage("but you continue to do the same thing... now sorry is a sorry word, but it's all I have for you");  }
-}
