3687514dc085b459473d8cef52471216519c801b
Cameron Lund <cameronlund4@gmail.com>
2017-09-20 17:43:47 -0400

    Cleaned up lambda commands Lambda commands added to a static helper to clean up Main.java

--- src/main/java/club/checs/csbot/Main.java
+++ src/main/java/club/checs/csbot/Main.java
@@ -1,20 +1,12 @@
 package club.checs.csbot;
 
-import club.checs.csbot.commands.SmartCommand;
-import club.checs.csbot.commands.implementations.*;
-import club.checs.csbot.commands.implementations.vexcommands.AwardsCommand;
-import club.checs.csbot.commands.implementations.vexcommands.RankCommand;
-import club.checs.csbot.commands.implementations.vexcommands.TeamCommand;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseCredentials;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
-import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RateLimitException;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -77,51 +69,7 @@ public class Main {
         cmanager = new CommandManager(client, pmanager);
 
         // Set up commands
-        cmanager.addCommand("boldtest", new TestCommand("boldtest"));
-        cmanager.addCommand("error", new SmartCommand("error", (call) -> {
-            @SuppressWarnings("NumericOverflow") int i = 10 / 0;
-        }));
-        cmanager.addCommand("ryan", new SmartCommand("ryan", (call) -> {
-            try {
-                call.getSender().getOrCreatePMChannel().sendMessage("LOL nerd");
-            } catch (MissingPermissionsException | RateLimitException | DiscordException e) {
-                e.printStackTrace();
-            }
-        }));
-        cmanager.addCommand("help", new SmartCommand("help", (call) -> {
-            StringBuilder builder = new StringBuilder().append("*Commands:* ");
-            for (SmartCommand command : cmanager.getCommands().values())
-                builder.append(command.getCommand()).append(", ");
-            call.sendMessage(builder.toString().substring(0, builder.toString().length() - 2));
-        }));
-        cmanager.addCommand("ryan", new SmartCommand("ryan", (call) -> {
-            call.sendMessage("***Look, it has too much give!***");
-        }));
-        cmanager.addCommand("backwards", new BackwardsCommand("backwards"));
-        cmanager.addCommand("badword", new CensorCommand("badword"));
-        cmanager.addCommand("censor", new CensorCommand("censor"));
-        cmanager.addCommand("translate", new TranslateCommand("translate"));
-        cmanager.addCommand("usertest", new UserCommand("usertest", client));
-        cmanager.addCommand("awards", new AwardsCommand("awards"));
-        cmanager.addCommand("team", new TeamCommand("team"));
-        cmanager.addCommand("rank", new RankCommand("rank"));
-        cmanager.addCommand("mute", new MuteCommand("mute", client, pmanager));
-        cmanager.addCommand("unmute", new UnmuteCommand("unmute", client, pmanager));
-        cmanager.addCommand("lambda", new LambdaAddCommand("lambda"));
-
-        // Utility/dev commands
-        cmanager.addCommand("getguild", new SmartCommand("getguild", (call) -> {
-            call.sendMessage("Guild ID: " + call.getEvent().getGuild().getStringID());
-        }));
-        cmanager.addCommand("getroles", new SmartCommand("getroles", (call) -> {
-            StringBuilder builder = new StringBuilder();
-            for (IRole role : call.getEvent().getGuild().getRolesForUser(call.getSender()))
-                builder.append(role).append(": ").append(role.getStringID()).append(" ");
-            call.sendMessage("Roles: " + builder.toString());
-        }));
-        cmanager.addCommand("getchannel", new SmartCommand("getchannel", (call) -> {
-            call.sendMessage("Channel ID: " + call.getEvent().getChannel().getStringID());
-        }));
+        LambdaCommandHelper.registerLambdaCommands(cmanager, pmanager, client);
 
         // Register command listener
         dispatcher.registerListener(cmanager); // Registers the command manager's listener
