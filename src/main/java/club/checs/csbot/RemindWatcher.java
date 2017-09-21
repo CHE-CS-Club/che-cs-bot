d30a16276272ee5ea606d6b3bf17c40ca5634dcb
Michael Ryan <michael_ryan_2000@yahoo.com>
2017-09-21 16:52:39 -0400

    Switched remind request to POST request
    Also allowed HttpsRequest to work for GET or POST requests.  Let’s just
    hope we don’t need PATCH or PUT for a little while, am I right?

--- src/main/java/club/checs/csbot/RemindWatcher.java
+++ src/main/java/club/checs/csbot/RemindWatcher.java
@@ -12,45 +12,26 @@ import java.util.HashMap;
 
 public class RemindWatcher {
     public RemindWatcher() {
-        System.out.println(
-                getResponse(sendHttpRequest("https://api.remind.com:443/v2/access_tokens", "user[email]",
-                        Keystore.getKey("quizletUser"), "user[password]",
-                        Keystore.getKey("quizletPass"))).toString());
+        JsonObject response = getResponse(new HttpsRequest(HttpsRequest.reqType.POST, "https://api.remind.com:443/v2/access_tokens",
+                "user[email]", Keystore.getKey("quizletUser"),
+                "user[password]", Keystore.getKey("quizletPass")));
+        System.out.println(response);
     }
 
-    public HttpsRequest sendHttpRequest(String url, HashMap<String, String> args) {
+    public HttpsRequest sendHttpRequest(HttpsRequest.reqType type, String url, HashMap<String, String> args) {
         String[] stringArgs = new String[args.size() * 2];
         // Add the key and value to the array in order
         for (int i = 0; i < args.keySet().size(); i++) {
             stringArgs[i * 2] = (String) args.keySet().toArray()[0];
             stringArgs[(i * 2) + 1] = args.get(stringArgs[i]);
         }
-        return sendHttpRequest(url, stringArgs);
-    }
-
-    public HttpsRequest sendHttpRequest(String url, String... args) {
-        // Make sure we have valid args
-        if ((args.length & 1) != 0) // If number is odd
-            throw new InvalidParameterException("Need even number of arguments");
-        // Go through the keys/values and build the arg string with them
-        StringBuilder argString = new StringBuilder();
-        for (int i = 0; i < args.length; i += 2) {
-            argString.append((i == 0) ? "?" : "&");
-            argString.append(args[i]).append("=");
-            try {
-                argString.append(URLEncoder.encode(args[i + 1], "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-        System.out.println("URL: " + url + argString.toString());
-        // Build the request and return it
-        return new HttpsRequest(url + argString.toString());
+        return new HttpsRequest(type, url, stringArgs);
     }
 
     private JsonObject getResponse(HttpsRequest req) {
-        JsonObject res = new JsonParser().parse(req.getRawResponse()).getAsJsonObject();
-        return res;
+        String rawResponse = req.getRawResponse();
+        if (rawResponse == null)
+            return null;
+        return new JsonParser().parse(req.getRawResponse()).getAsJsonObject();
     }
 }
--- src/main/java/club/checs/csbot/bot/HttpsRequest.java
+++ src/main/java/club/checs/csbot/bot/HttpsRequest.java
@@ -1,15 +1,19 @@
 package club.checs.csbot.bot;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.security.InvalidParameterException;
+
+import static club.checs.csbot.bot.HttpsRequest.reqType.POST;
 
 public class HttpsRequest {
 
+    public enum reqType {GET, POST}
     private URL url;
+    private reqType requestType;
 
     public HttpsRequest(String url) {
         try {
@@ -17,28 +21,96 @@ public class HttpsRequest {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        this.requestType = reqType.GET;
+    }
+
+    public HttpsRequest(String url, reqType type) {
+        try {
+            this.url = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        this.requestType = type;
+    }
+
+    public HttpsRequest(reqType type, String url, String... args) {
+        // Make sure we have valid args
+        if ((args.length & 1) != 0) // If number is odd
+            throw new InvalidParameterException("Need even number of arguments");
+        // Go through the keys/values and build the arg string with them
+        StringBuilder argString = new StringBuilder();
+        for (int i = 0; i < args.length; i += 2) {
+            try {
+                argString.append((i == 0) ? "?" : "&");
+                argString.append(URLEncoder.encode(args[i], "UTF-8")).append("=");
+                argString.append(URLEncoder.encode(args[i + 1], "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("URL: " + url + argString.toString());
+        // Build the request and set it
+        try {
+            this.url = new URL(url + argString.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        this.requestType = type;
     }
 
     public String getRawResponse() {
-        try {
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-            BufferedReader br =
-                    new BufferedReader(
-                            new InputStreamReader(conn.getInputStream()));
+        switch (requestType) {
+            case GET : {
+                try {
+                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                    return readBufferedInput(conn.getInputStream());
 
-            String input, output = "";
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            case POST : {
+                try {
+                    // Setup connection
+                    URL baseUrl = new URL(url.getProtocol() + "://" + url.getAuthority() + url.getPath()); // Remove args for initial connection
+                    HttpsURLConnection conn = (HttpsURLConnection) baseUrl.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setDoOutput(true);
 
-            while ((input = br.readLine()) != null)
-                output += input;
+                    // Setup and send the query portion of the url
+                    byte[] query = url.getQuery().getBytes();
+                    conn.setFixedLengthStreamingMode(query.length);
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    conn.connect();
+                    try(OutputStream os = conn.getOutputStream()) {
+                        os.write(query);
+                    }
 
-            br.close();
-            conn.getInputStream().close();
-            return output;
-
-        } catch (IOException e) {
-            e.printStackTrace();
+                    // Read the buffered input stream
+                    return readBufferedInput(conn.getInputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
         return null;
+    }
+
+    String readBufferedInput (InputStream stream) throws IOException {
+        BufferedReader br =
+                new BufferedReader(
+                        new InputStreamReader(stream));
+
+        String input, output = "";
+
+        while ((input = br.readLine()) != null)
+            output += input;
+
+        br.close();
+        stream.close();
+        return output;
     }
 
 
